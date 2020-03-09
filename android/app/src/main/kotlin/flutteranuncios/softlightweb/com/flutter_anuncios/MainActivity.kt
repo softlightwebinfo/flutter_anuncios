cbf2513136e61d09973310904b563063c78d95fe
Rafa Gonzalez <softlightwebinfo@gmail.com>
2020-03-09 17:36:50 +0100

    Initial

---
+++ android/app/src/main/kotlin/flutteranuncios/softlightweb/com/flutter_anuncios/MainActivity.kt
@@ -0,0 +1,12 @@
+package flutteranuncios.softlightweb.com.flutter_anuncios
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
